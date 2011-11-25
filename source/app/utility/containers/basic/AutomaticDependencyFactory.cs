d3f3eb138d350dd972049b7a6e4549162bf92f6f
kenk <kenk@iqmetrix.com>
2011-11-25 14:42:02 -0600

    Pushing new changes

--- source/app/utility/containers/basic/AutomaticDependencyFactory.cs
+++ source/app/utility/containers/basic/AutomaticDependencyFactory.cs
@@ -1,10 +1,36 @@
-﻿namespace app.utility.containers.basic
+﻿using System;
+using System.Collections.Generic;
+
+namespace app.utility.containers.basic
 {
   public class AutomaticDependencyFactory : ICreateADependency
   {
-    public object create()
-    {
-      throw new System.NotImplementedException();
-    }
+  	IFetchDependencies fetcher;
+  	IPickTheConstructorForAType iPicker;
+  	Type my_type;
+
+  	public AutomaticDependencyFactory(IFetchDependencies fetcher, IPickTheConstructorForAType iPicker, Type myType)
+  	{
+  		this.fetcher = fetcher;
+  		this.iPicker = iPicker;
+  		my_type = myType;
+  	}
+
+  	public object create()
+  	{
+  		var ctor = iPicker.get_applicable_constructor_on(my_type);
+  		List<object> parms = new List<object>();
+
+		// for each parameter in the constructor, fetch its dependency.
+		foreach (var param in ctor.GetParameters())
+		{
+			var dependency = fetcher.an(param.ParameterType);
+			parms.Add(dependency);
+			// somehow go back and set the value on myType...??
+		}
+
+  		return ctor.Invoke(parms.ToArray());
+  	}
+
   }
 }
