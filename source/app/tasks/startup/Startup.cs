e359fc50258668db1c259356dd9936b7614c61cb
kenk <kenk@iqmetrix.com>
2011-11-25 14:58:12 -0600

    Pushing new changes

--- source/app/tasks/startup/Startup.cs
+++ source/app/tasks/startup/Startup.cs
@@ -11,40 +11,43 @@ using app.web.core.stubs;
 
 namespace app.tasks.startup
 {
-  public class Startup
-  {
-    static IDictionary<Type, ICreateADependency> dependencies;
-    static IFetchDependencies container;
+	public class Startup
+	{
+		static IDictionary<Type, ICreateADependency> dependencies;
+		static IFetchDependencies container;
 
-    public static void run()
-    {
-      dependencies = new Dictionary<Type, ICreateADependency>();
-      container = new DependencyContainer(dependencies);
-      ContainerFacadeResolution facade_resolution = () => container;
-      Container.facade_resolution = facade_resolution;
+		public static void run()
+		{
+			dependencies = new Dictionary<Type, ICreateADependency>();
+			container = new DependencyContainer(dependencies);
+			ContainerFacadeResolution facade_resolution = () => container;
+			Container.facade_resolution = facade_resolution;
 
-      wire_everything_up();
-    }
+			wire_everything_up();
+		}
 
-    static void wire_everything_up()
-    {
-      register<IBuildRequestMatchers, RequestMatchBuilder>();
-      register<IFindPathsToViews, StubPathRegistry>();
+		static void wire_everything_up()
+		{
+			register<IBuildRequestMatchers, RequestMatchBuilder>();
+			register<IFindPathsToViews, StubPathRegistry>();
+			register<ICreateAResponse, PageFactory>();
+			register<IDisplayReportModels, WebResponseEngine>();
+			register<IFindCommands, CommandRegistry>();
+			register<ICreateRequests, StubRequestFactory>();
+			register<IProcessOneRequest>(() => Stub.with<StubMissingCommand>());
+			register<CurrentContextResolver>(() => HttpContext.Current);
+			register<IEnumerable<IProcessOneRequest>>(() => Stub.with<StubSetOfCommands>());
+			
+			register<IProcessRequests>(() => new FrontController(Container.fetch.an<IFindCommands>()));
+		}
 
-      register<ICreateAResponse>(()=>new PageFactory(Container.fetch.an<IFindPathsToViews>(), BuildManager.CreateInstanceFromVirtualPath));
-      register<IDisplayReportModels>(() => new WebResponseEngine(Container.fetch.an<ICreateAResponse>(), () => HttpContext.Current));
-      register<IFindCommands>(() => new CommandRegistry(Stub.with<StubSetOfCommands>(), Stub.with<StubMissingCommand>()));
-      register<ICreateRequests>(Stub.with<StubRequestFactory>);
-      register<IProcessRequests>(() => new FrontController(Container.fetch.an<IFindCommands>()));
-    }
-
-    static void register<Contract,Implementation>()
-    {
-      dependencies.Add(typeof(Contract), new AutomaticDependencyFactory(container, new GreedyConstructorSelectionStrategy()));
-    }
-    static void register<RegisteredType>(Func<object> factory_method)
-    {
-      dependencies.Add(typeof(RegisteredType), new SimpleFactory(factory_method));
-    }
-  }
+		static void register<Contract, Implementation>()
+		{
+			dependencies.Add(typeof(Contract), new AutomaticDependencyFactory(container, new GreedyConstructorSelectionStrategy(), typeof(Implementation)));
+		}
+		static void register<RegisteredType>(Func<object> factory_method)
+		{
+			dependencies.Add(typeof(RegisteredType), new SimpleFactory(factory_method));
+		}
+	}
 }
